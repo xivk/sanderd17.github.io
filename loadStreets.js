d1144af801e75b47dc650c2a84fc07098fbd0635
Sander Deryckere <sanderd17@gmail.com>
2014-10-20 21:51:29 +0200

    It would be a good idea to commit everything

--- loadStreets.js
+++ loadStreets.js
@@ -1,17 +1,52 @@
+// vim: tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab
 
 var overpassapi = "http://overpass-api.de/api/interpreter?data=";
 
 var crabInfo = {};
 var osmInfo = [];
 var missingAddr;
+var missingNoPosAddr;
 var wrongAddr;
-var streets; // var filled by the inline script in the pcode.html page
+var streets = {};
 var finished = {};
+// Get the pcode from the URL query
+var pcode = window.location.search.substring(1,5);
+
+var tableId = "streetsTable"
+
+function addStreetList()
+{
+	var req = new XMLHttpRequest();
+	req.overrideMimeType("application/json");
+	req.onreadystatechange = function()
+	{
+		if (req.readyState != 4)
+			return;
+		var data = JSON.parse(req.responseText);
+		streets = data.streets;
+		var html = "";
+		for (var i = 0; i < streets.length; i++)
+		{
+			var street = streets[i];
+			var idPart = pcode + '-' + street.sanName;
+			html += '<tr id="' + idPart + '">\n';
+			html += '<td id="' + idPart + '-name">\n' + street.name + '\n</td>\n';
+			html += '<td id="' + idPart + '-total"></td>\n';
+			html += '<td id="' + idPart + '-missing"></td>\n';
+			html += '<td id="' + idPart + '-missing-nopos"></td>\n';
+			html += '<td id="' + idPart + '-wrong"></td>\n';
+			html += '</tr>\n';
+		}
+		document.getElementById(tableId).innerHTML += html;
+	}
+	req.open("GET", pcode + ".json", true);
+	req.send(null);
+}
 
 /**
  * Add the street info for that certain streetname and pcode to the context object
  */
-function addCrabInfo(sanName, pcode, crabInfo) {
+function getCrabInfo(sanName) {
 	var req = new XMLHttpRequest();
 	req.overrideMimeType("application/json");
 	req.onreadystatechange = function()
@@ -20,66 +55,60 @@ function addCrabInfo(sanName, pcode, crabInfo) {
 			return;
 		var data = JSON.parse(req.responseText);
 
-		if (!crabInfo[data.pcode])
-			crabInfo[pcode] = {};
-		crabInfo[pcode][sanName] = data.addresses;
+		crabInfo[sanName] = data.addresses;
 		finished[sanName] = true;
-		finishLoading(pcode);
+		finishLoading();
 	};
 	req.open("GET", pcode + "/" + sanName + ".json", true);
 	req.send(null);
 }
 
 
-function createDocument(pcode, id)
+function updateData()
 {
-	var html = '<h1>Address import ' + pcode + '</h1>\n';
-	html += '<table id="' + pcode + '-table">\n';
-	html += '<tr><th>Name</th><th>Total</th><th>Missing</th><th>Wrong</th></tr>\n';
+	crabInfo = {};
 	for (var i = 0; i < streets.length; i++)
 	{
 		var street = streets[i];
-		html += '<tr id="' + pcode + '-' + street.sanName + '">\n';
-		html += '<td id="' + pcode + '-' + street.sanName + '-name">\n' + street.name + '\n</td>\n';
-		html += '<td id="' + pcode + '-' + street.sanName + '-total"></td>\n';
-		html += '<td id="' + pcode + '-' + street.sanName + '-missing"></td>\n';
-		html += '<td id="' + pcode + '-' + street.sanName + '-wrong"></td>\n';
-		html += '</tr>\n';
+		var idPart = pcode + '-' + street.sanName;
+		document.getElementById(idPart + "-total").innerHTML += "Loading...";
+		document.getElementById(idPart + "-missing").innerHTML += "Loading...";
+		document.getElementById(idPart + "-missing-nopos").innerHTML += "Loading...";
+		document.getElementById(idPart + "-wrong").innerHTML += "Loading...";
 		// Also import the actual CRAB data
 		finished[street.sanName] = false;
-		addCrabInfo(street.sanName, pcode, crabInfo);
+		getCrabInfo(street.sanName);
 	}
-	document.getElementById(id).innerHTML = html;
 	// Load osm data
 	finished["#osm"] = false;
-	getOsmInfo(pcode, osmInfo);
+	getOsmInfo();
 }
 
 /**
  * Check if everything is loaded, then finish everything
  */
-function finishLoading(pcode)
+function finishLoading()
 {
 	for (var k in finished)
 		if (!finished[k])
 			return;
-	compareData(pcode);
+	compareData();
 }
 
 /**
  * Get the data from osm, ret should be an empty array
  */
-function getOsmInfo(pcode, osmInfo) {
-	var query = [
-		'[out:json];',
-		'area["boundary"="administrative"]["addr:postcode"="' + pcode + '"]->.area;',
-		'(',
-			'node["addr:housenumber"](area.area);',
-			'way["addr:housenumber"](area.area);',
-			'relation["addr:housenumber"](area.area);',
-		');',
+function getOsmInfo() {
+	var query = 
+		'[out:json];'+
+		'area["boundary"="administrative"]["addr:postcode"="' + pcode + '"]->.area;'+
+		'('+
+			'node["addr:housenumber"](area.area);'+
+			'way["addr:housenumber"](area.area);'+
+			'relation["addr:housenumber"](area.area);'+
+		');'+
 		'out center;'
-	].join("");
+
 	var req = new XMLHttpRequest();
 	req.onreadystatechange = function()
 	{
@@ -91,26 +120,19 @@ function getOsmInfo(pcode, osmInfo) {
 		for (var i = 0; i < data.length; i++)
 		{
 			var addr = {};
-			if (data[i].lat)
-			{
-				addr.lat = data[i].lat;
-				addr.lon = data[i].lon;
-			}
-			else if (data[i].center && data[i].center.lat)
-			{
-				addr.lat = data[i].center.lat;
-				addr.lon = data[i].center.lon;
-			}
-			else
+			var d = data[i];
+			addr.lat = d.lat || d.center.lat;
+			addr.lon = d.lon || d.center.lon;
+
+			// TODO support Associated Street relations as valid?
+			if (!d.tags["addr:housenumber"] || !d.tags["addr:street"])
 				continue;
-			if (!data[i].tags["addr:housenumber"] || !data[i].tags["addr:street"])
-				continue;
-			addr.housenumber = data[i].tags["addr:housenumber"];
-			addr.street = data[i].tags["addr:street"];
+			addr.housenumber = d.tags["addr:housenumber"];
+			addr.street = d.tags["addr:street"];
 			osmInfo.push(addr);
 		}
 		finished["#osm"] = true;
-		finishLoading(pcode);
+		finishLoading();
 	}
 	req.open("GET", overpassapi + encodeURIComponent(query), true);
 	req.send(null);
@@ -119,41 +141,59 @@ function getOsmInfo(pcode, osmInfo) {
 /**
  * This function assumes all crab data and the osm data is loaded
  */
-function compareData(pcode) {
+function compareData() {
 	missingAddr = {};
+	missingNoPosAddr = {};
 	wrongAddr = {};
 	for (var i = 0; i < streets.length; i++)
 	{
 		var street = streets[i];
-		if (!missingAddr[pcode])
-		{
-			missingAddr[pcode] = {};
-			wrongAddr[pcode] = {};
-		}
 
 		// get the list with all housenumbers in this street from the two sources
-		var crabStreet = crabInfo[pcode][street.sanName];
+		var crabStreet = crabInfo[street.sanName];
 		var re = new RegExp("^" + street.name.replace(".",".*") + "$");
 		var osmStreet = osmInfo.filter(function(addr) {
 			return re.test(addr.street);
 		});
 		
 		// Matches in one direction
-		missingAddr[pcode][street.sanName] = compareHnr(crabStreet, osmStreet);
-		wrongAddr[pcode][street.sanName] = compareHnr(osmStreet, crabStreet);
+		var crabStreetPos = crabStreet.filter(function(addr) {
+			return addr.lat && addr.lon;
+		});
+		var crabStreetNoPos = crabStreet.filter(function(addr) {
+			return !addr.lat || !addr.lon;
+		});
+		missingAddr[street.sanName] = compareHnr(crabStreetPos, osmStreet);
+		missingNoPosAddr[street.sanName] = compareHnr(crabStreetNoPos, osmStreet);
+		wrongAddr[street.sanName] = compareHnr(osmStreet, crabStreet);
 
 
-		// Create links like
-		// <a href="URI" download="file.osm">n</a>
-		var dataURI =  getOsmURI(crabInfo[pcode][street.sanName]);
-		document.getElementById(pcode + '-' + street.sanName + '-total').innerHTML = 
-			'<a href="' + dataURI + '" download="' + street.sanName + '_full.osm">' + crabStreet.length + '</a>';
-		dataURI =  getOsmURI(missingAddr[pcode][street.sanName]);
-		document.getElementById(pcode + '-' + street.sanName + '-missing').innerHTML = 
-			'<a href="' + dataURI + '" download="' + street.sanName + '_missing.osm">' + missingAddr[pcode][street.sanName].length + '</a>';
-		dataURI =  getOsmURI(wrongAddr[pcode][street.sanName]);
-		document.getElementById(pcode + '-' + street.sanName + '-wrong').innerHTML = 
-			'<a href="' + dataURI + '" download="' + street.sanName + '_wrong.osm">' + wrongAddr[pcode][street.sanName].length + '</a>';
+		// Create links
+		var getHtml = function(num, obj, street, layerSuffix, msg)
+		{
+			if (msg)
+				msg = '"' + msg + '"';
+			return "<a href='#' title='Load this data in JOSM' onclick='openInJosm(%obj[\"%street\"], streets[%i], \"%layerName\", %msg)' >%num</a>"
+				.replace("%obj", obj)
+				.replace("%street", street)
+				.replace("%i", i)
+				.replace("%layerName", street + layerSuffix)
+				.replace("%msg", msg)
+				.replace("%num", num);
+		}
+		var idPart = pcode + '-' + street.sanName;
+
+		document.getElementById(idPart + '-total').innerHTML = 
+			getHtml(crabStreet.length, "crabInfo", street.sanName, "-full");
+
+		document.getElementById(idPart + '-missing').innerHTML = 
+			getHtml(missingAddr[street.sanName].length, "missingAddr", street.sanName, "-missing");
+
+		document.getElementById(idPart + '-missing-nopos').innerHTML = 
+			getHtml(missingNoPosAddr[street.sanName].length, "missingNoPosAddr", street.sanName, "-missing-noPos", "Position not found in CRAB");
+
+		document.getElementById(idPart + '-wrong').innerHTML = 
+			getHtml(wrongAddr[street.sanName].length, "wrongAddr", street.sanName, "-wrong", "Housenumber not found in Crab");
 	}
 }
 
@@ -172,24 +212,37 @@ function compareHnr(source, comp) {
 	return diffList;
 }
 
-function getOsmURI(data, message)
+function openInJosm(data, streetData, layerName, message)
 {
 	var timeStr = (new Date()).toISOString();
-	var str = 'data:text/xml;charset=utf-8,';
-	str += "<?xml version='1.0' encoding='utf-8'?>";
-	str += "<osm version='0.6' generator='flanders-addr-import'>\n";
+	var str = "<osm version='0.6' generator='flanders-addr-import'>\n";
 	for (var i = 0; i < data.length; i++)
 	{
 		var addr = data[i];
-		if (!addr.lat)
-			continue;
-		str += "  <node id='" + (-i-1) + "' lat='" + addr.lat + "' lon='" + 3.2346396 + "' version='0' timestamp='" + timeStr + "' uid='1' user=''>\n";
-		str += "    <tag k='addr:housenumber' v='" + addr.housenumber + "'/>\n";
-		str += "    <tag k='addr:street' v='" + addr.street + "'/>\n";
+		// take the precise position when available, else, center on the street
+		var lat = addr.lat;
+		var lon = addr.lon;
+		if (!lat || !lon)
+		{
+			lat = (streetData.latmax + streetData.latmin) / 2
+			lon = (streetData.lonmax + streetData.lonmin) / 2
+		}
+
+		str += "<node id='" + (-i-1) + "' lat='" + lat + "' lon='" + lon + "' version='0' timestamp='" + timeStr + "' uid='1' user=''>";
+		str += "<tag k='addr:housenumber' v='" + addr.housenumber + "'/>";
+		str += "<tag k='addr:street' v='" + addr.street + "'/>";
 		if (message)
-			str += "    <tag k='fixme' v='" + message + "'/>\n";
-		str += "  </node>\n";
+			str += "<tag k='fixme' v='" + message + "'/>";
+		str += "</node>\n";
 	}
 	str += "</osm>\n";
-	return str;
+
+	var url =  "http://localhost:8111/load_data?new_layer=true&layer_name="+layerName+"&data=";
+	console.log(str);
+	var req = new XMLHttpRequest();
+	window.open(url + encodeURIComponent(str));
 }
+
+// EXECUTE
+document.title = pcode + " Addr Import";
+addStreetList();
